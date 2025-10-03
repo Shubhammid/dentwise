c7d6f41b5387c830c00ea80a1adc7c01ea32a5ad
Shubham Midgule <shubham.midgule17@gmail.com>
2025-10-03 21:03:28 +0530

    Enhance layout with new components and update global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+     appearance={{
+          variables: {
+            colorPrimary: "#e78a53",
+            colorBackground: "#f3f4f6",
+            colorText: "#111827",
+            colorTextSecondary: "#6b7280",
+            colorInputBackground: "#f3f4f6",
+          },
+        }}
+    >
       <html lang="en">
-        <body className={`${lato.className} antialiased`}>{children}</body>
+        <body className={`${lato.className} antialiased dark`}>{children}</body>
       </html>
     </ClerkProvider>
   );
